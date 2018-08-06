710f91afe2a81a56360da0941e21edbd466e2827
Rafal Pienkowski <rafal.pienkowski@wp.pl>
2018-08-06 11:35:04 +0200

    AddEnvironmentVariables added to configuration builder.

--- src/BuildTrigger.Web/Startup.cs
+++ src/BuildTrigger.Web/Startup.cs
@@ -31,6 +31,12 @@ namespace BuildTrigger.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json")
+                .AddEnvironmentVariables()
+                .Build();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
